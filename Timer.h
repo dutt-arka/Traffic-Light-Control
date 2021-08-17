98a4a079952fb2682eaa27a2028ce51c9e8f4727
Arka Dutta <35682899+dutt-arka@users.noreply.github.com>
2021-08-17 12:44:24 +0530

    Create Timer.h

---
+++ Timer.h
@@ -0,0 +1,20 @@
+#ifndef _TIMER_H_
+#define _TIMER_H_
+
+class Timer{
+    long timerStart = 0;
+    long timerTarget = 0;
+public:
+  Timer(){}
+
+  void startTimer(int msec) {
+      timerStart = millis();
+      timerTarget = msec;
+  }
+  bool isTimerReady() {
+      return (millis() - timerStart) > timerTarget;
+  }
+  
+};
+
+#endif // _TIMER_H_
